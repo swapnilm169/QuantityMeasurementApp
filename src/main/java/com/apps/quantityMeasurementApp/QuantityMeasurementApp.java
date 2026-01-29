e7492b55ebe41a110e291bfb2ddd6502ddc09323
swapnilm169 <swap.mane16@gmail.com>
2026-01-29 11:36:24 +0530

    inital setup

---
+++ src/main/java/com/apps/quantityMeasurementApp/QuantityMeasurementApp.java
@@ -0,0 +1,4 @@
+package com.apps.quantityMeasurementApp;
+
+public class QuantityMeasurementApp {
+}
---
+++ src/test/java/com/apps/quantityMeasurementAppTest/QuantityMeasurementAppTest.java
@@ -0,0 +1,4 @@
+package com.apps.quantityMeasurementAppTest;
+
+public class QuantityMeasurementAppTest {
+}
