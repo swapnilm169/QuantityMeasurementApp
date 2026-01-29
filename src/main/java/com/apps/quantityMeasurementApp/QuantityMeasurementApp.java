51aab02c5e34707f78dab59cacc5b8e1cb74ece0
swapnilm169 <swap.mane16@gmail.com>
2026-01-29 13:58:31 +0530

    logic added

--- src/main/java/com/apps/quantityMeasurementApp/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantityMeasurementApp/QuantityMeasurementApp.java
@@ -1,4 +1,41 @@
 package com.apps.quantityMeasurementApp;
 
+import java.util.Scanner;
+
 public class QuantityMeasurementApp {
+    public static class Feet{
+        private final Double value;
+
+        public Feet(Double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this==obj) return true;
+            if(obj == null||getClass()!= obj.getClass())return false;
+            Feet feet =(Feet)obj;
+            if(this.value== null && feet.value==null){
+                return true;
+            }
+            if(this.value== null || feet.value==null){
+                return false;
+            }
+            return Double.compare(this.value,feet.value)==0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner value_One= new Scanner(System.in);
+        System.out.println("Enter First value ::");
+
+        Scanner value_Two= new Scanner(System.in);
+        System.out.println("Enter Second value ::");
+
+        QuantityMeasurementApp.Feet scan_val1= new  Feet(Double.parseDouble(value_One.nextLine()));
+        QuantityMeasurementApp.Feet scan_val2= new  Feet(Double.parseDouble(value_Two.nextLine())) ;
+
+        boolean result= scan_val1.equals(scan_val2);
+        System.out.println(result ? "Equal(true)" :"not-Equal(false)");
+    }
 }
--- src/test/java/com/apps/quantityMeasurementAppTest/QuantityMeasurementAppTest.java
+++ src/test/java/com/apps/quantityMeasurementAppTest/QuantityMeasurementAppTest.java
@@ -1,4 +1,57 @@
 package com.apps.quantityMeasurementAppTest;
 
+import com.apps.quantityMeasurementApp.QuantityMeasurementApp;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class QuantityMeasurementAppTest {
+    @Test
+    public void testFeetEquality_SameValue(){
+        QuantityMeasurementApp.Feet feet_value_1 = new QuantityMeasurementApp.Feet( 10d);
+        QuantityMeasurementApp.Feet feet_value_2 = new QuantityMeasurementApp.Feet(10d);
+        var result= feet_value_1.equals(feet_value_2);
+        assert (result);
+    }
+    @Test
+    public void testFeetEquality_DifferentValue(){
+        QuantityMeasurementApp.Feet feet_value_1 = new QuantityMeasurementApp.Feet(10d);
+        QuantityMeasurementApp.Feet feet_value_2 = new QuantityMeasurementApp.Feet(11d);
+        var result= feet_value_1.equals(feet_value_2);
+        assertFalse(result);
+    }
+    @Test
+    public void testFeetEquality_NullComparison_1ValueNull(){
+        QuantityMeasurementApp.Feet feet_value_1 = new QuantityMeasurementApp.Feet(null);
+        QuantityMeasurementApp.Feet feet_value_2 = new QuantityMeasurementApp.Feet(10d);
+        var result= feet_value_1.equals(feet_value_2);
+        assertFalse(result);
+    }
+    @Test
+    public void testFeetEquality_NullComparison_2ValueNull(){
+        QuantityMeasurementApp.Feet feet_value_1 = new QuantityMeasurementApp.Feet(10d);
+        QuantityMeasurementApp.Feet feet_value_2 = new QuantityMeasurementApp.Feet(null);
+        var result= feet_value_1.equals(feet_value_2);
+        assertFalse(result);
+    }
+    @Test
+    public void testFeetEquality_NullComparisonAllValueNull(){
+        QuantityMeasurementApp.Feet feet_value_1 = new QuantityMeasurementApp.Feet(null);
+        QuantityMeasurementApp.Feet feet_value_2 = new QuantityMeasurementApp.Feet(null);
+        var result= feet_value_1.equals(feet_value_2);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testFeetEquality_SameReference(){
+        QuantityMeasurementApp.Feet feet_value_1 = new QuantityMeasurementApp.Feet(10d);
+        QuantityMeasurementApp.Feet feet_value_null = feet_value_1;
+        var result= feet_value_1.equals(feet_value_null);
+        assertTrue(result);
+    }
+    @Test
+    public void testFeetEquality_DifferentClass(){
+        QuantityMeasurementApp.Feet feet_value_1 = new QuantityMeasurementApp.Feet(10d);
+        Object obj="D1";
+        assertThrows(ClassCastException.class, () -> new QuantityMeasurementApp.Feet((Double) obj));
+    }
 }
