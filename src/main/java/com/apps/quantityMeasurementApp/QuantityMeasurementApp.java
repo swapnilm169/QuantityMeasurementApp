c6b5976ce7b361d1800e90eeb2eefda905a0d0be
swapnilm169 <swap.mane16@gmail.com>
2026-01-30 14:24:00 +0530

    UC2 logic added

--- src/main/java/com/apps/quantityMeasurementApp/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantityMeasurementApp/QuantityMeasurementApp.java
@@ -1,4 +1,64 @@
 package com.apps.quantityMeasurementApp;
 
 public class QuantityMeasurementApp {
+    public static class Feet {
+        private final Double value;
+
+        public Feet(Double value) {
+            this.value = value;
+        }
+        @Override
+        public boolean equals(Object obj) {
+            if (this==obj) return true;
+            if(obj == null||getClass()!= obj.getClass())return false;
+            Feet feet =(Feet)obj;
+            if(this.value== null || feet.value==null){
+                return false;
+            }
+            return Double.compare(this.value,feet.value)==0;
+        }
+    }
+        public static class Inches {
+            private final Double value;
+
+            public Inches(Double value) {
+                this.value = value;
+            }
+            @Override
+            public boolean equals(Object obj) {
+                if (this==obj) return true;
+                if(obj == null||getClass()!= obj.getClass())return false;
+                Inches inches =(Inches)obj;
+                if(this.value == null || inches.value==null){
+                    return false;
+                }
+                return Double.compare(this.value,inches.value)==0;
+            }
+        }
+        public  static void demonstrateFeetEquality(){
+            Feet feet_first_Val= new Feet(1.0d);
+            Inches inches_first_Val=new  Inches(12.0d);
+
+            Double converted =  convertInchedToFeet(inches_first_Val);
+
+            boolean result_feet= feet_first_Val.equals(new Feet(converted));
+            System.out.println(result_feet ? " Feet ::Equal(true)" :" Feet ::: not-Equal(false)");
+        }
+
+        private static double convertInchedToFeet(Inches inchesConverted){
+            return inchesConverted.value / 12;
+        }
+
+        public  static void demonstrateInchesEquality(){
+            QuantityMeasurementApp.Inches inches_first_Val= new QuantityMeasurementApp.Inches(12.0d);
+            QuantityMeasurementApp.Inches inches_second_Val=new  QuantityMeasurementApp.Inches(12.0d);
+
+            boolean result_Inches= inches_first_Val.equals(inches_second_Val);
+            System.out.println(result_Inches ? "Inches :: Equal(true)" :"Inches:: not-Equal(false)");
+        }
+
+    public static void main(String[] args) {
+            demonstrateFeetEquality();
+            demonstrateInchesEquality();
+    }
 }
--- src/test/java/com/apps/quantityMeasurementAppTest/QuantityMeasurementAppTest.java
+++ src/test/java/com/apps/quantityMeasurementAppTest/QuantityMeasurementAppTest.java
@@ -1,4 +1,109 @@
 package com.apps.quantityMeasurementAppTest;
 
+import com.apps.quantityMeasurementApp.QuantityMeasurementApp;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class QuantityMeasurementAppTest {
+    @Test
+    public void testFeetEquality_SameValue(){
+        QuantityMeasurementApp.Feet feet_value_1 = new QuantityMeasurementApp.Feet( 10d);
+        QuantityMeasurementApp.Feet feet_value_2 = new QuantityMeasurementApp.Feet(10d);
+        var result= feet_value_1.equals(feet_value_2);
+        assert (result);
+    }
+    @Test
+    public void testFeetEquality_DifferentValue(){
+        QuantityMeasurementApp.Feet feet_value_1 = new QuantityMeasurementApp.Feet(10d);
+        QuantityMeasurementApp.Feet feet_value_2 = new QuantityMeasurementApp.Feet(11d);
+        var result= feet_value_1.equals(feet_value_2);
+        assertFalse(result);
+    }
+    @Test
+    public void testFeetEquality_NullComparison_1ValueNull(){
+        QuantityMeasurementApp.Feet feet_value_1 = new QuantityMeasurementApp.Feet(null);
+        QuantityMeasurementApp.Feet feet_value_2 = new QuantityMeasurementApp.Feet(10d);
+        var result= feet_value_1.equals(feet_value_2);
+        assertFalse(result);
+    }
+    @Test
+    public void testFeetEquality_NullComparison_2ValueNull(){
+        QuantityMeasurementApp.Feet feet_value_1 = new QuantityMeasurementApp.Feet(10d);
+        QuantityMeasurementApp.Feet feet_value_2 = new QuantityMeasurementApp.Feet(null);
+        var result= feet_value_1.equals(feet_value_2);
+        assertFalse(result);
+    }
+    @Test
+    public void testFeetEquality_NullComparisonAllValueNull(){
+        QuantityMeasurementApp.Feet feet_value_1 = new QuantityMeasurementApp.Feet(null);
+        QuantityMeasurementApp.Feet feet_value_2 = new QuantityMeasurementApp.Feet(null);
+        var result= feet_value_1.equals(feet_value_2);
+        assertFalse(result);
+    }
+
+    @Test
+    public void testFeetEquality_SameReference(){
+        QuantityMeasurementApp.Feet feet_value_1 = new QuantityMeasurementApp.Feet(10d);
+        QuantityMeasurementApp.Feet feet_value_null = feet_value_1;
+        var result= feet_value_1.equals(feet_value_null);
+        assertTrue(result);
+    }
+    @Test
+    public void testFeetEquality_DifferentClass(){
+        QuantityMeasurementApp.Feet feet_value_1 = new QuantityMeasurementApp.Feet(10d);
+        Object obj="D1";
+        assertThrows(ClassCastException.class, () -> new QuantityMeasurementApp.Feet((Double) obj));
+    }
+
+    /*For Inches*/
+    @Test
+    public void testInchesEquality_SameValue(){
+        QuantityMeasurementApp.Inches inches_value_1 = new QuantityMeasurementApp.Inches( 10d);
+        QuantityMeasurementApp.Inches inches_value_2 = new QuantityMeasurementApp.Inches(10d);
+        var result= inches_value_1.equals(inches_value_2);
+        assert (result);
+    }
+    @Test
+    public void testInchesEquality_DifferentValue(){
+        QuantityMeasurementApp.Inches inches_value_1 = new QuantityMeasurementApp.Inches(10d);
+        QuantityMeasurementApp.Inches inches_value_2 = new QuantityMeasurementApp.Inches(11d);
+        var result= inches_value_1.equals(inches_value_2);
+        assertFalse(result);
+    }
+    @Test
+    public void testInchesEquality_NullComparison_1ValueNull(){
+        QuantityMeasurementApp.Inches inches_value_1 = new QuantityMeasurementApp.Inches(null);
+        QuantityMeasurementApp.Inches inches_value_2 = new QuantityMeasurementApp.Inches(10d);
+        var result= inches_value_1.equals(inches_value_2);
+        assertFalse(result);
+    }
+    @Test
+    public void testInchesEquality_NullComparison_2ValueNull(){
+        QuantityMeasurementApp.Inches inches_value_1 = new QuantityMeasurementApp.Inches(10d);
+        QuantityMeasurementApp.Inches inches_value_2 = new QuantityMeasurementApp.Inches(null);
+        var result= inches_value_1.equals(inches_value_2);
+        assertFalse(result);
+    }
+    @Test
+    public void testInchesEquality_NullComparisonAllValueNull(){
+        QuantityMeasurementApp.Inches inches_value_1 = new QuantityMeasurementApp.Inches(null);
+        QuantityMeasurementApp.Inches inches_value_2 = new QuantityMeasurementApp.Inches(null);
+        var result= inches_value_1.equals(inches_value_2);
+        assertFalse(result);
+    }
+
+    @Test
+    public void testInchesEquality_SameReference(){
+        QuantityMeasurementApp.Inches inches_value_1 = new QuantityMeasurementApp.Inches(10d);
+        QuantityMeasurementApp.Inches inches_value_null = inches_value_1;
+        var result= inches_value_1.equals(inches_value_null);
+        assertTrue(result);
+    }
+    @Test
+    public void testInchesEquality_DifferentClass(){
+        QuantityMeasurementApp.Inches inches_value_1 = new QuantityMeasurementApp.Inches(10d);
+        Object obj="D1";
+        assertThrows(ClassCastException.class, () -> new QuantityMeasurementApp.Inches((Double) obj));
+    }
 }
